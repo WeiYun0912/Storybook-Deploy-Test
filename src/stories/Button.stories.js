36091dc3043e5256cd75329823b274d806ba4b08
ChunWeiYeah <yher25@gmail.com>
2022-07-01 14:44:31 +0800

    first commit

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,8 +1,44 @@
 import Button from "../components/Button";
 
 export default {
-  title: "Button",
+  title: "Components/Button",
   component: Button,
+  argTypes: { handleClick: { action: "handleClick" } },
 };
 
-export const Red = () => <Button label="Press Me" backgroundColor="red" />;
+const Template = (args) => <Button {...args} />;
+
+export const Red = Template.bind({});
+Red.args = {
+  backgroundColor: "red",
+  label: "Press Me",
+  size: "md",
+};
+
+export const Green = Template.bind({});
+Green.args = {
+  backgroundColor: "green",
+  label: "Press Me",
+  size: "md",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  backgroundColor: "red",
+  label: "Press Me",
+  size: "sm",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  backgroundColor: "red",
+  label: "Press Me",
+  size: "lg",
+};
+
+export const LongLabel = Template.bind({});
+LongLabel.args = {
+  backgroundColor: "red",
+  label: "Press Me adsf asdf asdf asdfasdfasd fasd fasd fasd",
+  size: "md",
+};
